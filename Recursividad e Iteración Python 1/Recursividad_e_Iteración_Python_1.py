c01984e039a233a4ff3b6ea5dd47aa8dfbfe4015
leong <leong@DESKTOP-A61DB78>
2023-10-27 20:55:38 -0400

    Agregar archivos de proyecto.

---
+++ Recursividad e Iteración Python 1/Recursividad_e_Iteración_Python_1.py
@@ -0,0 +1,11 @@
+NUM = int(input("DIGITE NÚMERO: "))
+
+I = 1
+
+while I <= 12:
+    RESUL = NUM * I
+    print(f"{NUM} x {I} = {RESUL}")
+    I = I + 1
+
+input("Pulse una Tecla: ")  # Para esperar una entrada del usuario antes de cerrar el programa
+
